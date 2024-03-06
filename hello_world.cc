d56c3732d5c7c016a67c9a103570113b98e020b3
HaiFanNeon <haifan0312@163.com>
2024-03-06 20:54:46 +0800

    bbbb

--- hello_world.cc
+++ hello_world.cc
@@ -6,6 +6,7 @@ int main()
     {
         std::cout << "hello github" << std::endl;
     }
-    std::cout << "aaa" << std::endl; 
+    std::cout << "aaa" << std::endl;
+    std::cout << "bbbb" << std::endl;
     return 0;
 }
